7a9b44861f9196f4c555cf5f66c3bd537592eb2e
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2022-05-26 15:49:17 +0100

    update

--- navigator.c
+++ navigator.c
@@ -46,13 +46,49 @@ char *get_current_highlighted_file(char path[], int *user_position, char highlig
 
 }
 
-void print_current_dir(char path[], int *user_position, char full_path[], char highlighted_name[], int *current_highlighted_file_is_dir)
+void center_vertically()
+{
+    int i;
+    for(i = 0; i < 5; i++)
+        printf("\n");
+}
+
+void count_children_files(char path[], char folder_name[], int *file_count)
+{
+    DIR *d;
+    struct dirent *dir;
+    int i = 0;
+//    printf("path: %s\n", path);
+//    printf("folder_name: %s\n", folder_name);
+    char full_path[PATH_MAX];
+    strcpy(full_path, path);
+    strcat(full_path, "/");
+    strcat(full_path, folder_name);
+    d = opendir(full_path);
+    while ((dir = readdir(d)) != NULL)
+    {
+        if(*file_count < 9)
+        {
+            if((strcmp(dir->d_name, ".")))
+                if((strcmp(dir->d_name, "..")))
+                {
+                    *file_count += 1;
+                    i++;
+                    //printf("%s\n", dir->d_name);
+                }
+        }
+
+    }
+}
+
+void print_current_dir(char path[], int *user_position, char full_path[], char highlighted_name[], int *current_highlighted_file_is_dir, int *min_visible_files, int range_visible)
 {
     DIR *d;
     struct dirent *dir;
     int i = 0;
     int user_position_upper_bound = 0;
     int user_position_lower_bound = 1;
+    int file_count = 0;
     d = opendir(path);
     if (d == NULL)
     {
@@ -84,28 +120,38 @@ void print_current_dir(char path[], int *user_position, char full_path[], char h
             if((strcmp(dir->d_name, "..")))
             {
                 i++;
-                //ANYTHING PRINTED AFTER WILL BE RED
-                printf("\033[0;31m");
-                //yellow on blue background
-                printf("\033[33;44m");
-                //default colors
-                printf("\033[0m");
 
-
-                if(i == *user_position)
+                if(i <= range_visible)
                 {
+                    //ANYTHING PRINTED AFTER WILL BE RED
                     printf("\033[0;31m");
-
-                }
-                else 
+                    //yellow on blue background
+                    printf("\033[33;44m");
+                    //default colors
                     printf("\033[0m");
 
-                if(dir->d_type == 4)
-                    printf("\033[44;44m");
 
+                    if(i == *user_position)
+                    {
+                        printf("\033[0;31m");
 
-                printf("%s\n", dir->d_name);
-                printf("\033[0m");
+                    }
+                    else 
+                        printf("\033[0m");
+
+                    if(dir->d_type == 4)
+                    {
+                        //count_children_files(path, dir->d_name, &file_count);
+                        printf("\033[44;44m");
+                        printf("%-2d %s %-*s %d\n", i, ":", 45, dir->d_name, file_count);
+                    }
+                    else 
+                    {
+                        printf("%-2d %s %-*s\n", i, ":", 45, dir->d_name);
+                    }
+                        printf("\033[0m");
+                    file_count = 0;
+                }
             }
         }
     }
@@ -154,15 +200,19 @@ int main (void)
     char full_path[PATH_MAX];
     char highlighted_name[PATH_MAX];
     int current_highlighted_file_is_dir = 1;
+    int min_visible_files = 0;
+    int range_visible = 45;
+
     char c;
 
     hide_cursor();
     clear();
+    center_vertically();
     getcwd(cwd, sizeof(cwd));
     printf("----------------\n");
     printf("%s\n", cwd);
     printf("----------------\n");
-    print_current_dir(cwd, &user_position, full_path, highlighted_name, &current_highlighted_file_is_dir);
+    print_current_dir(cwd, &user_position, full_path, highlighted_name, &current_highlighted_file_is_dir, &min_visible_files, range_visible);
     while((c = getchar()) != EOF)
     {
         clear();
@@ -187,10 +237,21 @@ int main (void)
         {
             user_position--;
         }
+        if(c == 'g')
+        {
+            const char* s = getenv("HOME");
+            strcpy(cwd, s);
+        }
+        if(c == '/')
+        {
+            strcpy(cwd, "/");
+        }
+        center_vertically();
         printf("----------------\n");
         printf("%s\n", cwd);
         printf("----------------\n");
-        print_current_dir(cwd, &user_position, full_path, highlighted_name, &current_highlighted_file_is_dir);
+        print_current_dir(cwd, &user_position, full_path, highlighted_name, &current_highlighted_file_is_dir, &min_visible_files, range_visible);
+
     }
 
 
